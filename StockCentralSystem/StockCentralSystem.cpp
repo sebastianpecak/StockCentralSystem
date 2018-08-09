d517043d27002fccdf3ad0f84fae7ff9fea96017
Sebastian Pęcak <sebastian_pecak@mennica.com.pl>
2018-08-09 12:23:04 +0200

    Add project files.

---
+++ StockCentralSystem/StockCentralSystem.cpp
@@ -0,0 +1,12 @@
+﻿// StockCentralSystem.cpp : Defines the entry point for the application.
+//
+
+#include "StockCentralSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
